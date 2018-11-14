69c4fa538633b4048940baee4c4ff0305581e2c2
tiancechen <chen0650@algonquinlive.com>
2018-11-14 17:13:30 -0500

    test

--- app/src/main/java/com/example/ruoruo/finalproject/OCTranspo.java
+++ app/src/main/java/com/example/ruoruo/finalproject/OCTranspo.java
@@ -1,19 +1,94 @@
 package com.example.ruoruo.finalproject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class OCTranspo extends Activity {
 
+
     protected static final String ACTIVITY_NAME = "OCTranspo";
+    private Button byRoute;
+    private Button byStop;
+    private EditText userEnter;
+    private Button goHomeO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_octranspo);
         Log.i(ACTIVITY_NAME, "In onCreate()");
+
+         Button byRoute = (Button)findViewById(R.id.byRoute);
+         Button byStop = (Button)findViewById(R.id.byStop);
+         EditText userEnter = (EditText)findViewById(R.id.userEnter);
+         Button goHomeO = (Button)findViewById(R.id.goHomeO);
+
+        Toast.makeText(getApplicationContext(), "message", Toast.LENGTH_SHORT).show();
+
+        byRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make( v, "message", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        byStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make( v, "message", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+
+        goHomeO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog dialog = new AlertDialog.Builder(OCTranspo.this)
+                        .setTitle("Notice!")
+                        .setMessage("You are leaving the page!")
+
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                dialog.dismiss();
+                            }
+                        })
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(OCTranspo.this,MainActivity.class);
+                                startActivity(intent);
+                                dialog.dismiss();
+                            }
+                        }).create();
+                dialog.show();
+            }
+        });
+
+
+
+
+
+
     }
+
+
 
     @Override
     protected void onResume() {

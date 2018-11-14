e5dddf7667a6f9615e87a5b59ed155a62a90fa58
Jamesbarclay <barc0060@algonquinlive.com>
2018-11-14 17:52:36 -0500

    Added one Button, One Toast, One SnackBar, One ListView, and one EditText

--- app/src/main/java/com/example/ruoruo/finalproject/Nutrition.java
+++ app/src/main/java/com/example/ruoruo/finalproject/Nutrition.java
@@ -1,17 +1,37 @@
 package com.example.ruoruo.finalproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Nutrition extends Activity {
 
     protected static final String ACTIVITY_NAME = "Nutrition";
 
+    Button buttonclick;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrition);
+
+        buttonclick = findViewById(R.id.clickableButton);
+
+        Toast.makeText(Nutrition.this,"Nutrition is the best!", Toast.LENGTH_LONG).show();
+
+        buttonclick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make( v, "What a nice Snack bar, so much nutrition", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
         Log.i(ACTIVITY_NAME, "In onCreate()");
     }
 

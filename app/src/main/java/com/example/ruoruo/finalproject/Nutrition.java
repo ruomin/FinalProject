fffce529d40251bfc4e55dadf0796caa89c7e447
Jamesbarclay <barc0060@algonquinlive.com>
2018-11-22 04:02:08 -0500

    Milestone 2

--- app/src/main/java/com/example/ruoruo/finalproject/Nutrition.java
+++ app/src/main/java/com/example/ruoruo/finalproject/Nutrition.java
@@ -7,17 +7,26 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.Switch;
 
-public class Nutrition extends Activity {
+import android.support.v7.widget.Toolbar;
+
+public class Nutrition extends AppCompatActivity {
 
     protected static final String ACTIVITY_NAME = "Nutrition";
 
     Button buttonclick;
     Button Exit;
+    Button Fragment;
+    public Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +35,17 @@ public class Nutrition extends Activity {
 
         buttonclick = findViewById(R.id.clickableButton);
         Exit = findViewById(R.id.clickableButton2);
+        Fragment = findViewById(R.id.fragmentButton);
+        toolbar = findViewById(R.id.newToolBar);
+        setSupportActionBar(toolbar);
 
-        Toast.makeText(Nutrition.this,"Nutrition is the best!", Toast.LENGTH_LONG).show();
+
+        Toast.makeText(Nutrition.this, "Nutriment is the best!", Toast.LENGTH_LONG).show();
 
         buttonclick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make( v, "What a nice Snack bar, so much nutrition", Snackbar.LENGTH_LONG).show();
+                Snackbar.make(v, "What a nice Snack bar, so much nutriment", Snackbar.LENGTH_LONG).show();
             }
         });
 
@@ -40,10 +53,22 @@ public class Nutrition extends Activity {
             @Override
             public void onClick(View v) {
                 showDialog();
+                Toast.makeText(Nutrition.this, "Exit to Main Application page", Toast.LENGTH_LONG).show();
             }
         });
 
+        Fragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentPage fragmentPage = new FragmentPage();
+                getFragmentManager().beginTransaction().replace(R.id.frameLayout, fragmentPage).commit();
+            }
+        });
+
+
         Log.i(ACTIVITY_NAME, "In onCreate()");
+
+
     }
 
     private void showDialog() {
@@ -67,29 +92,72 @@ public class Nutrition extends Activity {
         dialog.show();//show the dialog
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(ACTIVITY_NAME, "In onResume()");
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(ACTIVITY_NAME, "In onStart()");
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Intent intent;
+        AlertDialog.Builder builder;
+        //   Dialog dialog = new AlertDialog.Builder(this);
+        switch (id) {
+
+            case R.id.nutritionItem:
+                // intent = new Intent(this, Nutrition.class);
+                // startActivity(intent);
+                break;
+
+            case R.id.movieNutrition:
+                intent = new Intent(this, Movie.class);
+                startActivity(intent);
+                break;
+
+            case R.id.helpNutrition:
+
+                builder = new AlertDialog.Builder(Nutrition.this);
+                builder.setMessage(R.string.Information);
+                builder.setTitle(R.string.Title)
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .create();//create the dialog
+                builder.show();
+        }
+        return true;
     }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(ACTIVITY_NAME, "In onPause()");
+
+
+
+
+        @Override
+        protected void onResume () {
+            super.onResume();
+            Log.i(ACTIVITY_NAME, "In onResume()");
+        }
+        @Override
+        protected void onStart () {
+            super.onStart();
+            Log.i(ACTIVITY_NAME, "In onStart()");
+        }
+        @Override
+        protected void onPause () {
+            super.onPause();
+            Log.i(ACTIVITY_NAME, "In onPause()");
+        }
+        @Override
+        protected void onStop () {
+            super.onStop();
+            Log.i(ACTIVITY_NAME, "In onStop()");
+        }
+        @Override
+        protected void onDestroy () {
+            super.onDestroy();
+            Log.i(ACTIVITY_NAME, "In onDestroy()");
+        }
     }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(ACTIVITY_NAME, "In onStop()");
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(ACTIVITY_NAME, "In onDestroy()");
-    }
-}
+

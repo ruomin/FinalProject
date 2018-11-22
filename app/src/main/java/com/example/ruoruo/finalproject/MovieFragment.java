6e9bdf4c6558fdb70a076718c18900a77e963eee
“[ruomin <[zhan0484@algonquinlive.com]>
2018-11-21 21:51:54 -0500

    R.Z

--- app/src/main/java/com/example/ruoruo/finalproject/MovieFragment.java
+++ app/src/main/java/com/example/ruoruo/finalproject/MovieFragment.java
@@ -2,12 +2,30 @@ package com.example.ruoruo.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MovieFragment extends Activity {
+
+public class MovieFragment extends Fragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_fragment);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,   ViewGroup container, Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.activity_movie_fragment, container, false);
+
+
+
+        return null;
+
     }
 }
